88829b54bacd4b77c6da402299e8c92338282d29
VRomanenko04 <vovasan2004@gmail.com>
2023-02-20 17:31:59 +0200

    Небольшие правки слайдера

--- js/timing-slider.js
+++ js/timing-slider.js
@@ -4,11 +4,10 @@ const activeSlide = document.querySelector('.slider-img.active');
 
 function changeSlide() {
     let nextSlide = activeSlide.nextElementSibling;
-
+    
     if (!nextSlide) {
         nextSlide = slides[0];
     }
-
     activeSlide.classList.remove('active');
     nextSlide.classList.add('active');
 }
