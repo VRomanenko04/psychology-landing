2599c76966ced7f974644b5499fb0b4eebf2704e
VRomanenko04 <vovasan2004@gmail.com>
2023-02-20 16:00:45 +0200

    Сделал основу под слайдер

---
+++ js/timing-slider.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slider-img');
+const activeSlide = document.querySelector('.slider-img.active');
+
+
+function changeSlide() {
+    let nextSlide = activeSlide.nextElementSibling;
+
+    if (!nextSlide) {
+        nextSlide = slides[0];
+    }
+
+    activeSlide.classList.remove('active');
+    nextSlide.classList.add('active');
+}
+
+setInterval(changeSlide, 3000);
