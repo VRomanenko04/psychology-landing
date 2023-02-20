488d33ccf247c7685d172ce5dddc9011787333fa
VRomanenko04 <vovasan2004@gmail.com>
2023-02-21 01:09:01 +0200

    Сделана страница обо мне, с js

--- js/text-animation.js
+++ js/text-animation.js
@@ -1,3 +1,4 @@
+//Активация анимации текста на первой странице
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
@@ -11,3 +12,19 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenText = document.querySelectorAll('.showing-text');
 hiddenText.forEach((el) => observer.observe(el));
+
+
+//Активация анимации текста на второй странице
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-text');
+        } else {
+            entry.target.classList.remove('show-text');
+        }
+    });
+});
+
+const hiddenText2 = document.querySelectorAll('.about-item');
+hiddenText2.forEach((el) => observer2.observe(el));
