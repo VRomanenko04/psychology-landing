c49ddbc269518351f09070b915b8ee33721ad150
VRomanenko04 <vovasan2004@gmail.com>
2023-02-28 01:41:43 +0200

    Изменение структуры, цвета лого, текста, анимаций

--- js/text-animation.js
+++ js/text-animation.js
@@ -1,19 +1,3 @@
-//Активация анимации текста на первой странице
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-
-const hiddenText = document.querySelectorAll('.showing-text');
-hiddenText.forEach((el) => observer.observe(el));
-
-
 //Активация анимации текста на второй странице
 const observer2 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
