8d83d6f69c56802a2f94051d78a7aee2f30063df
VRomanenko04 <vovasan2004@gmail.com>
2023-02-28 23:16:03 +0200

    Подготовка переменных к новому слайдеру

--- js/timing-slider.js
+++ js/timing-slider.js
@@ -1,18 +1,18 @@
 //Создаём переменные с массивом слайдов и активным слайдом
 const slides = document.querySelectorAll('.slider-img');
-const activeSlide = document.querySelector('.slider-img.active');
+const activeSlide = document.querySelector('.slider-img.current');
 
 // Функция для смены класса у активного слайда и передвачи его следующему
 function changeSlide() {
-    const activeSlide = document.querySelector('.slider-img.active');
+    const activeSlide = document.querySelector('.slider-img.current');
     let nextSlide = activeSlide.nextElementSibling;
     
     // Если следующего слайда нету - возвращаемся к первому
     if (!nextSlide) {
         nextSlide = slides[0];
     }
-    activeSlide.classList.remove('active');
-    nextSlide.classList.add('active');
+    activeSlide.classList.remove('current');
+    nextSlide.classList.add('current');
 }
 
 // Зацикливаем функцию с интервалом в 3 сек
